3ffa2ba71c55279a77e503fa73b54f8bf080feea
daniel <kaluifesinachi@yahoo.com>
2026-01-13 18:38:33 +0100

    ederation governance patterns

---
+++ src/federation/mod.rs
@@ -0,0 +1,9 @@
+﻿//! Federation governance patterns
+
+/// Federation structure
+pub mod federation;
+/// Federation policies
+pub mod policy;
+
+pub use federation::Federation;
+pub use policy::{Policy, PolicyType};
