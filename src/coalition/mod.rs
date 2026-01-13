39b6c550ecdd45afb967d42b3e4818ea53a6402d
daniel <kaluifesinachi@yahoo.com>
2026-01-13 18:34:19 +0100

    Coalition formation patterns

---
+++ src/coalition/mod.rs
@@ -0,0 +1,12 @@
+﻿//! Coalition formation patterns
+
+/// Coalition structure
+pub mod coalition;
+/// Coalition formation algorithms
+pub mod formation;
+/// Coalition strategy
+pub mod strategy;
+
+pub use coalition::Coalition;
+pub use formation::{Formation, FormationType};
+pub use strategy::{Strategy, StrategyType};
