56abc7fabccccc8bb584f54e663f6a2f36d3fe90
daniel <kaluifesinachi@yahoo.com>
2026-01-13 18:22:28 +0100

    Blackboard architecture patterns

---
+++ src/blackboard/mod.rs
@@ -0,0 +1,9 @@
+﻿//! Blackboard architecture patterns
+
+/// Blackboard structure
+pub mod blackboard;
+/// Knowledge source
+pub mod knowledge_source;
+
+pub use blackboard::Blackboard;
+pub use knowledge_source::{KnowledgeSource, KnowledgeSourceType};
